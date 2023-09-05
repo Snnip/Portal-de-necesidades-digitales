37463da9afa7de9b61a77b40ee02131a8c686142
Wi-Go <akaza.xeros@gmail.com>
2023-09-05 19:22:13 +0200

    minor joi fixes

--- src/schemas/users/insertUserSchema.js
+++ src/schemas/users/insertUserSchema.js
@@ -2,15 +2,14 @@ const joi = require('joi');
 
 // Mensajes de error de Joi que necesitemos.
 const joiErrorMessages = {
-    'string.base': "El valor de '{#key}' debe ser una cadena de texto",
-    'any.required': "El campo '{#key}' es requerido",
-    'string.empty': "El campo '{#key}' no debe estar vacío",
+    'string.base': 'El valor de {#key} debe ser una cadena de texto',
+    'any.required': 'El campo {#key} es requerido',
+    'string.empty': 'El campo {#key} no debe estar vacío',
     'string.min':
         'El nombre de usuario debe tener al menos {#limit} caracteres',
-    'string.email':
-        "Debe proporcionar un correo electrónico válido para '{#key}'",
+    'string.email': 'Debe proporcionar un correo electrónico válido.',
     'string.pattern.base':
-        "La contraseña debe contener al menos una mayúscula, una minúscula, un número, un símbolo de puntuación y un mínimo de 8 caracteres para '{#key}'",
+        'La contraseña debe contener al menos una mayúscula, una minúscula, un número, un símbolo de puntuación y un mínimo de 8 caracteres.',
 };
 
 // Creamos el esquema de Joi
