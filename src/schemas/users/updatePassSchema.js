2044f73927e5c583892ac9ba46b35cede3bd81fb
SandraPol <sanpb10@gmail.com>
2023-09-12 22:45:32 +0200

    Mensajes de errores cambiados a inglés

--- src/schemas/users/updatePassSchema.js
+++ src/schemas/users/updatePassSchema.js
@@ -2,13 +2,12 @@ const joi = require('joi');
 
 // Mensajes de error de Joi que necesitemos.
 const joiErrorMessages = {
-    'string.base': 'El valor de {#key} debe ser un texto',
-    'any.required': 'El campo {#key} es requerido',
-    'string.empty': 'El campo {#key} no debe estar vacío',
-    'string.min':
-        'El nombre de usuario debe tener al menos {#limit} caracteres',
+    'string.base': 'The value of {#key} must be a text string',
+    'any.required': 'The field {#key} is required.',
+    'string.empty': 'The {#key} field must not be empty.',
+    'string.min': 'The user name must be at least {#limit} characters long.',
     'string.pattern.base':
-        'La contraseña debe contener al menos una mayúscula, una minúscula, un número, un símbolo de puntuación y un mínimo de 8 caracteres.',
+        'The password must contain at least one uppercase letter, one lowercase letter, one number, one punctuation symbol and a minimum of 8 characters.',
 };
 
 // Creamos el esquema de Joi
